a49d2577728a381f3508a973095b6c0e2bf6c1c2
Facundo Barneche <facu_barneche@hotmail.com.ar>
2021-03-17 14:51:57 -0300

    se hizo un modelo de footer

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 
 
 function App() {
   return (
     <div className="App">
    <Navbar/>
+
+   <img src="https://ep01.epimg.net/elpais/imagenes/2019/10/30/album/1572424649_614672_1572453030_noticia_normal.jpg"></img>
+   <Footer/>
     </div>
   );
 }
